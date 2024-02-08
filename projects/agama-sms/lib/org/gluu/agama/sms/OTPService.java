d558ca340fe424350acfb26012a0680ea37cf61a
gasmyr <gasmyrmougang@yahoo.fr>
2024-02-08 20:47:11 +0100

    feat: add OTPService.java

---
+++ projects/agama-sms/lib/org/gluu/agama/sms/OTPService.java
@@ -0,0 +1,16 @@
+package org.gluu.agama.sms;
+
+import org.gluu.agama.sms.jans.JansOTPService;
+
+public abstract class OTPService {
+
+    public abstract boolean validateCreds(String username, String password);
+
+    public abstract boolean sendOTPCode(String username, String userPhoneNumber);
+
+    public abstract boolean validateOTPCode(String username, String code);
+
+    private static OTPService getInstance(){
+        return  new JansOTPService();
+    }
+}
